0df3d9e0ea03021a96ac331553a9673f38a40476
Alex Antonison <adantonison@gmail.com>
2020-08-02 15:09:17 -0500

    Rework directory to include additional files

--- zip_files.sh
+++ zip_files.sh
@@ -2,6 +2,11 @@
 
 rm data_harvesters_project.zip
 
-
-
-zip -r data_harvesters_project.zip .
+zip data_harvesters_project.zip \
+    farmersmarkets_* \
+    Queries.txt \
+    Open* \
+    Overall_Workflow.* \
+    Python_Transformation.py \
+    Farmers_Market_DB.db \
+    load_table_Farmers_Market_Data.sql
